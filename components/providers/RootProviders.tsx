6c1257e71b46d16507c65b0b18b461fbb8254bb6
Baris Korkmaz <barskorkmaz6@gmail.com>
2024-11-11 15:27:17 +0300

    düzenlendi

--- components/providers/RootProviders.tsx
+++ components/providers/RootProviders.tsx
@@ -2,17 +2,22 @@
 
 import { ThemeProvider } from 'next-themes';
 import React, { ReactNode } from 'react'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
 function RootProviders({children}: {children:ReactNode}) {
+  const [queryClient] = React.useState(() => new QueryClient({}))
   return (
-
-    <ThemeProvider
-    attribute='class'
-    defaultTheme='dark'
-    enableSystem
-    disableTransitionOnChange>
+    <QueryClientProvider client = {queryClient}>
+     <ThemeProvider
+        attribute='class'
+        defaultTheme='dark'
+        enableSystem
+        disableTransitionOnChange>
         {children}
-    </ThemeProvider>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
