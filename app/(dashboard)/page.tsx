e9f66a7359b60f59899699b7ef60f1fcd9834176
Baris Korkmaz <barskorkmaz6@gmail.com>
2024-11-18 21:51:49 +0300

    ana sayfada gelir gider girme alanları oluşturuldu

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import prisma from '@/lib/prisma'
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+import React, { use } from 'react'
+import CreateTransactionDialog from './_components/CreateTransactionDialog'
 
-function page() {
+async function page() {
+
+  const user = await currentUser()
+  if (!user) {
+    redirect("/sign-in")
+  }
+
+  const userSettings = await prisma.userSettings.findUnique
+    ({
+      where: {
+        userId: user.id,
+      }
+    })
+
+  if (!userSettings) {
+    redirect("/wizard")
+  }
+
   return (
-    <div>
-      
+    <div className='h-full bg-background'>
+      <div className="border-b bg-card">
+        <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+          <p className="text-3xl font-bold pl-4">
+            Merhaba, {user.firstName}!
+          </p>
+
+          <div className="flex items-center gap-3 pr-4">
+            <CreateTransactionDialog
+              trigger={<Button
+                variant={"outline"}
+                className='border-emerald-500 bg-emerald-950 text-white 
+                hover:bg-emerald-700 hover:text-white'>
+                Gelir
+              </Button>
+              }
+              type="gelir"
+            />
+
+            <CreateTransactionDialog
+              trigger={
+                <Button
+                  variant={"outline"}
+                  className='border-rose-500 bg-rose-950 text-white 
+              hover:bg-rose-700 hover:text-white'>
+                  Gider
+                </Button>
+              }
+              type="gider"
+            />
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
