9682b383b9ee31d46a0ea7e9b726e574ac5e6706
Baris Korkmaz <barskorkmaz6@gmail.com>
2024-12-02 16:51:53 +0300

    düzenlendi

--- app/(dashboard)/_components/CreateCategoryDialog.tsx
+++ app/(dashboard)/_components/CreateCategoryDialog.tsx
@@ -10,7 +10,7 @@ import { cn } from '@/lib/utils';
 import { CreateCategorySchema, CreateCategorySchemaType } from '@/schema/categories';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { CircleOff, Loader2, PlusSquare } from 'lucide-react';
-import React, { useCallback, useState } from 'react'
+import React, { ReactNode, useCallback, useState } from 'react'
 import { useForm } from 'react-hook-form';
 import Picker from "@emoji-mart/react";
 import data from "@emoji-mart/data";
@@ -23,9 +23,10 @@ import { useTheme } from 'next-themes';
 interface Props {
     type: TransactionType;
     successCallback: (category: Category) => void;
+    trigger?: ReactNode;
 }
 
-function CreateCategoryDialog({ type, successCallback }: Props) {
+function CreateCategoryDialog({ type, successCallback, trigger }: Props) {
     const [open, setOpen] = useState(false);
     const form = useForm<CreateCategorySchemaType>({
         resolver: zodResolver(CreateCategorySchema),
@@ -76,11 +77,17 @@ function CreateCategoryDialog({ type, successCallback }: Props) {
 
     return <Dialog open={open} onOpenChange={setOpen}>
         <DialogTrigger asChild>
-            <Button variant={"ghost"} className='flex border-separate items-center 
-        justify-start roudned-none border-b px-3 py-3 text-muted-foreground'>
-                <PlusSquare className='mr-2 h-4 w-4' />
-                Yeni Oluştur
-            </Button>
+            {trigger ? (
+                trigger
+            ) : (
+                <Button
+                    variant={"ghost"}
+                    className='flex border-separate items-center 
+                    justify-start roudned-none border-b px-3 py-3 text-muted-foreground'>
+                    <PlusSquare className='mr-2 h-4 w-4' />
+                    Yeni Oluştur
+                </Button>
+            )}
         </DialogTrigger>
         <DialogContent>
             <DialogHeader>
